6efc7d3e0a157fbed267abcf64fb23424c111214
makaka86 <113855423+makaka86@users.noreply.github.com>
2022-09-19 13:41:45 +0300

    Update laba.java

--- laba.java
+++ laba.java
@@ -1,5 +1,3 @@
-package ru.itmo.prog.samples;
-
 import java.util.Arrays;
 import java.util.Random;
 
@@ -29,30 +27,34 @@ public class Main {
 
 
         for (int idx = 0; idx < arrayA.length; idx++)
-            for (int j = 0; j < arrayA[idx].length; j++)
-            {
+            for (int j = 0; j < arrayA[idx].length; j++) {
                 if (arrayX[idx] == 15)
                     arrayA[idx][j] = Math.pow((Math.cos(Math.pow((j / 2), j)) / 2), (Math.pow(((Math.pow((j * (j + 1)), 2) - 4) / (Math.cos(j))), 3)));
                 else if (arrayX[idx] == 7 | arrayX[idx] == 9 | arrayX[idx] == 11 | arrayX[idx] == 17 | arrayX[idx] == 23 | arrayX[idx] == 25)
-                    arrayA[idx][j] = Math.pow((1 - (Math.cos(Math.exp(Math.abs(j))))) / (Math.exp(Math.pow(Math.E ,j))) , 3);
+                    arrayA[idx][j] = Math.pow((1 - (Math.cos(Math.exp(Math.abs(j))))) / (Math.exp(Math.pow(Math.E, j))), 3);
                 else
-                    arrayA[idx][j] = Math.pow( (0.25 + Math.pow(Math.PI * Math.pow( 2 / (Math.pow(0.25 * (j - 0.25) ,j) + 2) , 2),3)) / (Math.pow( ( ( (Math.cbrt(Math.cbrt(j)) + 1) / 2) / 3),Math.exp(Math.pow(Math.cos(j) , 2)))), 2);
+                    arrayA[idx][j] = Math.pow((0.25 + Math.pow(Math.PI * Math.pow(2 / (Math.pow(0.25 * (j - 0.25), j) + 2), 2), 3)) / (Math.pow((((Math.cbrt(Math.cbrt(j)) + 1) / 2) / 3), Math.exp(Math.pow(Math.cos(j), 2)))), 2);
             }
         printArrA(arrayA);
     }
 
-    public static float genRandFloat()
-    {
+    public static float genRandFloat() {
         Random random = new Random();
         return random.nextFloat() * (13 - (-9)) + (-9);
     }
 
-    public static void printArrA(double[][] arr)
-    {
-        for (int i = 0; i < arr.length; i++)
-        {
-            for (int v = 0; v < arr[i].length; v++)
-                System.out.printf("%13.4f ",arr[i][v], " ");
+    public static void printArrA(double[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int v = 0; v < arr[i].length; v++) {
+                double arrNum = arr[i][v];
+                if (arrNum < 25000) {
+                    System.out.printf("%13.4f ", arrNum);
+                }
+                else {
+                    System.out.printf("%13.8s ", "Infinity");
+                }
+            }
+
             System.out.println();
         }
     }
